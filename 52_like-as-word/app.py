af62f31dbec4d0c8aa802b84631ed916f5fbbb46
veaba <908662421@qq.com>
2020-04-14 19:31:47 +0800

    update 文本相似度30%

--- 52_like-as-word/app.py
+++ 52_like-as-word/app.py
@@ -2,14 +2,38 @@ import jieba
 from gensim import corpora, models, similarities  # 文本相似度库gensim
 import pandas as pd
 
-train_docs_list = []
-test_docs_list = []
-with open("china-cdc-train_docs.txt", "r", encoding="utf-8") as f:
-    train_docs_list = [line.replace('\n', '') for line in f]
+# 三个空格隔开,才能被pd.read_csv读取到
+dataset_path = "./train-data.txt"
 
-with open("china-cdc-test_docs.txt", "r", encoding="utf-8") as f:
-    test_docs_list = [line.replace('\n', '') for line in f]
-#
+# 列名
+column_names = ["行数", "段落"]
+raw_dataset = pd.read_csv(dataset_path, names=column_names,
+                          na_values=" ", comment="\n",
+                          sep=" ", skipinitialspace=True)
+dataset = raw_dataset.copy()
+# print("全部数据===>\n", dataset)
+print("dataset.tail()==>\n", dataset.tail())  # 查看尾部的后五条数据
+# print("数据清洗==>\n", dataset.isna().sum())  # 不懂
+dataset.isna().sum()
+# 拆分数据集和测试集
+
+train_dataset = dataset.sample(frac=0.8, random_state=0)  # 8/10 训练集
+test_dataset = dataset.drop(train_dataset.index)  # 2/10 测试集
+
+print("数据集===>\n", train_dataset)
+print("测试集===>\n", test_dataset)
+
+print("行数===>\n", train_dataset["行数"])
+
+row_data = train_dataset["行数"]
+paragraph_data = train_dataset["段落"]
+for i in row_data:
+    print("i===>", i)
+
+print("行数的数组===>\n", list(row_data))
+print("段落的数组===>\n", list(paragraph_data))
+
+# todo 未完待续
 # # 对目标文档分词，保存在all_doc_list
 # train_docs_split = []
 # test_docs_split = []
